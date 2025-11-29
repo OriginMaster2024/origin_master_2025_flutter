321e18b3dab79ee2f840a41e223ce74cf53ca6ed
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2025-11-30 07:31:05 +0900

    Add ResultOverlay

---
+++ lib/result_overlay.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ResultOverlay extends StatelessWidget {
+  const ResultOverlay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
