2083dd9ad8a1de1f79f9b66d9694d1b218922913
nukotsuka <nukotsuka@gmail.com>
2025-11-29 18:05:11 +0900

    Refactor

--- lib/shooting_game.dart
+++ lib/shooting_game.dart
@@ -7,19 +7,22 @@ import 'turret.dart';
 
 class ShootingGame extends FlameGame {
   final Turret playerTurret = Turret(specs: TurretSpecs.getByLevel(1));
-  final Turret enemyTurret = Turret(specs: TurretSpecs.getByLevel(2), isEnemy: true);
+  final Turret enemyTurret = Turret(
+    specs: TurretSpecs.getByLevel(2),
+    isEnemy: true,
+  );
 
   double timeSinceLastShot = 0;
   double tiltX = 0;
   final double sensitivity = 20;
 
-  final BpmState? bpmState;
+  final BpmState bpmState;
 
   int turretLevel = 1; // 1:level1, 2:level2, 3:level3
   double stableCounter = 0.0; // isStable=true が続いた秒数
   double unstableCounter = 0.0; // isStable=false が続いた秒数
 
-  ShootingGame({this.bpmState});
+  ShootingGame({required this.bpmState});
 
   @override
   Future<void> onLoad() async {
@@ -63,21 +66,19 @@ class ShootingGame extends FlameGame {
     }
 
     // BPMデータを参照（例: 発射間隔の調整などに使用可能）
-    if (bpmState != null) {
-      // これらの値はゲームロジックで使用可能です
-      // ignore: unused_local_variable
-      final currentBpm = bpmState!.bpm;
-      // ignore: unused_local_variable
-      final isStable = bpmState!.isStable;
-      // ignore: unused_local_variable
-      final stdDev = bpmState!.stdDev;
-      // ここでBPMデータをゲームロジックに反映できます
-      // 例: 発射間隔の調整、難易度の変更など
-      // 使用例: playerTurret.specs.shotInterval = 0.5 + (currentBpm / 200.0);
-    }
+    // これらの値はゲームロジックで使用可能です
+    // ignore: unused_local_variable
+    final currentBpm = bpmState.bpm;
+    // ignore: unused_local_variable
+    final isStable = bpmState.isStable;
+    // ignore: unused_local_variable
+    final stdDev = bpmState.stdDev;
+    // ここでBPMデータをゲームロジックに反映できます
+    // 例: 発射間隔の調整、難易度の変更など
+    // 使用例: playerTurret.specs.shotInterval = 0.5 + (currentBpm / 200.0);
 
     // BPM 状態に応じたカウント
-    if (bpmState?.isStable == true) {
+    if (isStable) {
       stableCounter += dt;
       unstableCounter = 0.0;
 
