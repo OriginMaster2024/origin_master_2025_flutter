f53495f533b744c444b908938e16581ce57877b1
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2025-11-30 07:33:45 +0900

    Add body of overlay

--- lib/result_overlay.dart
+++ lib/result_overlay.dart
@@ -7,7 +7,46 @@ class ResultOverlay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      width: double.infinity,
+      color: Colors.black54,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'あなたの',
+                style: TextStyle(
+                    fontSize: 32,
+                    color: Colors.white,
+                    fontFamily: 'Melonano'
+                ),
+              ),
+              Text(
+                'かち！',
+                style: TextStyle(
+                    fontSize: 80,
+                    color: Colors.white,
+                    fontFamily: 'Melonano'
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 48),
+          GestureDetector(
+            onTap: () {
+            },
+            child: Image.asset(
+              'assets/button_back.png',
+              fit: BoxFit.fitWidth,
+              width: 142,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
