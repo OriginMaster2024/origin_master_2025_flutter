a244bd37b1bed2b0f755f291d0ed80d22e46631f
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2025-11-30 00:20:42 +0900

    Add battle button

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -46,7 +46,23 @@ class StartScreen extends StatelessWidget {
                         fit: BoxFit.fitWidth,
                         width: 142,
                       ),
-                    )
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        // GameWidget に遷移
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => GameScreen(gameID: Uuid().v4()),
+                          ),
+                        );
+                      },
+                      child: Image.asset(
+                        'assets/button_battle.png',
+                        fit: BoxFit.fitWidth,
+                        width: 142,
+                      ),
+                    ),
                   ],
                 ),
               ],
