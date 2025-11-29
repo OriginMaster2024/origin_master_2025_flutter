5a68dd11dd5eccf69067ef398d151d756c0ed290
Hatsune Mineta <hatsune.mineta@smartnews.com>
2025-11-29 15:32:46 +0900

    add enemy turret

--- lib/shooting_game.dart
+++ lib/shooting_game.dart
@@ -1,34 +1,37 @@
-import 'dart:ui';
-
-import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:sensors_plus/sensors_plus.dart';
-import 'bullet.dart';
-import 'turret_specs.dart';
+
+import 'turret.dart';
 
 class ShootingGame extends FlameGame {
-  late TurretSpecs turretSpecs;
-  late RectangleComponent turret;
+  late Turret playerTurret;
+  late Turret enemyTurret;
 
   double timeSinceLastShot = 0;
   double tiltX = 0;
   final double sensitivity = 10;
 
-  ShootingGame({required this.turretSpecs});
-
-  void setTurretSpecs(TurretSpecs specs) {
-    turretSpecs = specs;
-    turret.size = specs.size;
-  }
+  ShootingGame({
+    required TurretSpecs playerSpec,
+    required TurretSpecs enemySpec,
+  }) : playerTurret = Turret(specs: playerSpec),
+       enemyTurret = Turret(specs: enemySpec, isEnemy: true);
 
   @override
   Future<void> onLoad() async {
-    turret = RectangleComponent(
-      size: turretSpecs.size,
-      position: Vector2(size.x / 2 - turretSpecs.size.x / 2, size.y - turretSpecs.size.y - 20),
-      paint: Paint()..color = const Color(0xFF0000FF),
+    // プレイヤータレットの初期位置
+    playerTurret.position = Vector2(
+      size.x / 2 - playerTurret.specs.size.x / 2,
+      size.y - playerTurret.specs.size.y - 20,
     );
-    add(turret);
+    add(playerTurret);
+
+    // 敵タレットの固定位置（上部中央）
+    enemyTurret.position = Vector2(
+      size.x / 2 - enemyTurret.specs.size.x / 2,
+      50,
+    );
+    add(enemyTurret);
 
     accelerometerEventStream().listen((event) {
       tiltX = event.x;
@@ -39,19 +42,13 @@ class ShootingGame extends FlameGame {
   void update(double dt) {
     super.update(dt);
 
-    // 弾発射
-    timeSinceLastShot += dt;
-    if (timeSinceLastShot >= turretSpecs.shotInterval) {
-      timeSinceLastShot = 0;
-      final bulletPos = turret.position + Vector2(turret.size.x / 2 - 2.5, -20);
-      add(Bullet(bulletPos));
-    }
-
-    // 発射台移動
-    turret.x -= tiltX * sensitivity * dt;
+    // プレイヤータレットの移動
+    playerTurret.x -= tiltX * sensitivity * dt;
 
     // 画面端で制限
-    if (turret.x < 0) turret.x = 0;
-    if (turret.x + turret.size.x > size.x) turret.x = size.x - turret.size.x;
+    if (playerTurret.x < 0) playerTurret.x = 0;
+    if (playerTurret.x + playerTurret.specs.size.x > size.x) {
+      playerTurret.x = size.x - playerTurret.specs.size.x;
+    }
   }
 }
